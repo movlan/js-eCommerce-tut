2627209ad046e6aa45b4e9ffd5d3fd033e4d8ba6
Bahram Movlanov <bmovlanov@gmail.com>
2023-10-24 04:04:34 +0000

    add 2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Products from './components/Products/Products';
+
+const App = () => {
+  return (
+    <>
+      <div>eComerce</div>
+      <Products />
+    </>
+  );
+};
+
+export default App;
